bfc5ce53a9d66056fdc8ef8b97f9a392da87a218
Sivani-01 <219X1A2917@gprec.ac.in>
2026-01-21 21:44:02 +0530

    Create quotes.js

---
+++ quotes.js
@@ -0,0 +1,20 @@
+const quotes = {
+    easy: [
+        "The cat sat on the mat.",
+        "Life is a beautiful journey.",
+        "Code is poetry in motion.",
+        "Keep it simple and smart."
+    ],
+    medium: [
+        "The best way to predict the future is to create it.",
+        "Success is not final, failure is not fatal.",
+        "Don't count the days, make the days count.",
+        "Everything you've ever wanted is on the other side of fear."
+    ],
+    hard: [
+        "Linguistic diversity reflects the rich cultural heritage of civilizations.",
+        "Quantum mechanics provides a mathematical description of dual particle behavior.",
+        "The quintessential component of resilience is the ability to adapt.",
+        "Technological advancements accelerate the rate of global information exchange."
+    ]
+};
